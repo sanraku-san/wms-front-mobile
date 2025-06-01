d271153bc9341a8e91a1220ee78b17262f3ff80c
apeyt <abadpeyt@gmail.com>
2025-06-01 12:42:50 +0800

    local backend

--- app/api/configuration.js
+++ app/api/configuration.js
@@ -1,4 +1,2 @@
-
-export const URL = "https://apidominions.styxhydra.com/api";
-export const BASE_URL = "https://apidominions.styxhydra.com";
-export const LOCAL = "http://192.168.254.108:8000";
+export const URL = "http://192.168.1.16:8000/api";
+export const BASE_URL = "http://192.168.1.16:8000";
